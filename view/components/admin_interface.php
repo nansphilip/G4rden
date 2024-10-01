3984aad32153a45a52d083ddf40786ab5db7f1b6
theochevallier <theolemayne@gmail.com>
2024-10-01 21:13:46 +0200

    feat: add admin interface files

---
+++ view/components/admin_interface.php
@@ -0,0 +1,9 @@
+<!-- Delete a user -->
+ <div class="">
+    <h3>Supprimer un utilisateur</h3>
+    <p>Entrez le nom de l'utilisateur à supprimer.</p>
+    <input type="text" placeholder="Pseudo de l'utilisateur">
+    <button type="button" class="">Supprimer</button>
+ </div>
+
+<!-- Delete a message -->
