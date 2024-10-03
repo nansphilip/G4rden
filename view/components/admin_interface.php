ec86fd934d93e851ca7f4237720808374d70e48d
theochevallier <theolemayne@gmail.com>
2024-10-03 19:53:08 +0200

    feat: admin-interface

--- view/components/admin_interface.php
+++ view/components/admin_interface.php
@@ -1,9 +1,17 @@
 <!-- Delete a user -->
- <div class="">
-    <h3>Supprimer un utilisateur</h3>
-    <p>Entrez le nom de l'utilisateur à supprimer.</p>
-    <input type="text" placeholder="Pseudo de l'utilisateur">
-    <button type="button" class="">Supprimer</button>
- </div>
+<div class="justify-between margin-border">
+   <h3>Supprimer un utilisateur</h3>
+   <p>Entrez le nom de l'utilisateur à supprimer.</p>
+   <input type="text" class="margin" placeholder="Pseudo de l'utilisateur">
+   <button type="button" class="margin" id="deleteUser">Supprimer</button>
+</div>
+<div class="justify-between margin-border">
+   <h3>Supprimer un message</h3>
+   <p>Entrez le nom de l'utilisateur</p>
+   <input type="text" class="margin" placeholder="Pseudo de l'utilisateur">
+   <p>Entrez un mot clé du message à supprimer.</p>
+   <input type="text" class="margin" placeholder="Mot clé">
+   <button type="button" id="searchMessage">Rechercher</button>
+</div>
 
 <!-- Delete a message -->
