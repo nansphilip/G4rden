bd88c4aebb9f2534fe0d5305cd3c103d9e320f55
nansphilip <nansphilip@gmail.com>
2024-10-07 18:18:50 +0200

    fix: rename sql files

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,3 +1,15 @@
+-- Creates the database
+CREATE DATABASE `g4rden-db`;
+
+-- Selects the database
+USE `g4rden-db`;
+
+-- Creates the user and password
+CREATE USER 'g4rden-user' @'localhost' IDENTIFIED BY 'g4rden-password';
+
+-- Gives to the user all privileges on the database
+GRANT ALL PRIVILEGES ON `g4rden-db`.* TO 'g4rden-user'@'localhost';
+
 -- Creates user table
 CREATE TABLE `User` (
     `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
