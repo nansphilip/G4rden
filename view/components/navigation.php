2de7685def0b9fb1ff4e6b8b27c3874584063f55
nansphilip <nansphilip@gmail.com>
2024-10-08 19:16:12 +0200

    Fix header

--- view/components/navigation.php
+++ view/components/navigation.php
@@ -10,10 +10,8 @@
         echo "<a href={$PATH}/index.php?p=profile>Profile</a>";
         echo "<a href={$PATH}/index.php?p=logout>Logout</a>";
     } else {
-		?>
-        <a href=/index.php?p=login>Login</a>";
-        <a href=/index.php?p=register>Register</a>";
-	<?php
+        echo "<a href={$PATH}/index.php?p=login>Login</a>";
+        echo "<a href={$PATH}/index.php?p=register>Register</a>";
     }
     ?>
 </nav>
