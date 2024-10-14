38f2c2b417ee12d338e637690efae8db9d909299
nansphilip <nansphilip@gmail.com>
2024-10-14 08:58:14 +0200

    feat: dark mode almost done

--- static/js/global.js
+++ static/js/global.js
@@ -43,12 +43,12 @@ const toggleDarkMode = () => {
     documentAllElList.forEach((el) => {
         el.style.transition = "0ms";
     });
-    document.body.scrollHeight; // Forces chronological repaint
-    
+    document.body.scrollHeight; // Forces chronologicookiesListl repaint
+
     // Toggle dark mode
     rootEl.classList.toggle("dark");
-    document.body.scrollHeight; // Forces chronological repaint
-    
+    document.body.scrollHeight; // Forces chronologicookiesListl repaint
+
     // Restore transitions
     documentAllElList.forEach((el) => {
         el.style.transition = "";
@@ -58,13 +58,38 @@ const toggleDarkMode = () => {
     if (rootEl.classList.contains("dark")) {
         iconElList[0].style.display = "none";
         iconElList[1].style.display = "";
+        setCookie("darkTheme", "enabled");
     } else {
         iconElList[0].style.display = "";
         iconElList[1].style.display = "none";
+        destroyCookie("darkTheme");
     }
-
-    // Todo : send preference to server
 };
 
 // On click, toggle dark mode
 toggleButtonEl.addEventListener("click", toggleDarkMode);
+
+const setCookie = (name, value) => {
+    const cookieValue = name + "=" + value + ";";
+    const cookieExpires = "expires=" + new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000).toUTCString() + ";";
+    const cookiePath = "path=/;";
+    (document.cookie = cookieValue), cookieExpires, cookiePath;
+};
+
+const getCookie = (name) => {
+    const cookieName = name + "=";
+    return document.cookie.split(";").some((el) => {
+        const cookie = el.trim();
+        return cookie.indexOf(cookieName) === 0 && cookie.substring(cookieName.length) === "enabled";
+    });
+};
+
+const destroyCookie = (name) => {
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+};
+
+window.addEventListener("load", () => {
+    if (getCookie("darkTheme")) {
+        toggleDarkMode();
+    }
+});
